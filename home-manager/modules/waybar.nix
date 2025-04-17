a5e79b36412addb8a17fe7fd60f21e9238003640
mattephi <contact@mattephi.com>
2025-04-17 19:50:24 +0900

    feat: minimal waybar

--- home-manager/modules/waybar.nix
+++ home-manager/modules/waybar.nix
@@ -15,13 +15,36 @@
         output = [
           "DP-1"
         ];
-        modules-left = [ ];
+        modules-left = [ "hyprland/workspaces" ];
         modules-center = [ ];
         modules-right = [
-          "tray"
+          "hyprland/submap"
+          "hyprland/language"
+          "pulseaudio"
           "clock"
         ];
-
+        "pulseaudio" = {
+          format = "{volume}% {icon} {format_source}";
+          format-bluetooth = "{volume}% {icon} {format_source}";
+          format-bluetooth-muted = " {icon} {format_source}";
+          format-muted = " {format_source}";
+          format-source = "{volume}% ";
+          format-source-muted = "";
+          format-icons = {
+            headphone = "";
+            hands-free = "";
+            headset = "";
+            phone = "";
+            portable = "";
+            car = "";
+            default = [
+              ""
+              ""
+              ""
+            ];
+          };
+          on-click = "pavucontrol";
+        };
         "tray" = {
           spacing = 10;
         };
@@ -31,6 +54,35 @@
           interval = 1;
           format = "{:%H:%M:%S}";
           format-alt = "{:%Y-%m-%d}";
+        };
+        "hyprland/workspaces" = {
+          format = "<sub>{icon}</sub>";
+          format-window-separator = " ";
+        };
+      };
+      secondBar = {
+        layer = "top";
+        position = "top";
+        output = [
+          "HDMI-A-1"
+        ];
+        modules-left = [ "hyprland/workspaces" ];
+        modules-right = [
+          "tray"
+        ];
+        "clock" = {
+          # // "timezone": "America/New_York",
+          tooltip-format = "<big>{:%Y %B}</big>\n<tt><small>{calendar}</small></tt>";
+          interval = 1;
+          format = "{:%H:%M:%S}";
+          format-alt = "{:%Y-%m-%d}";
+        };
+        "tray" = {
+          spacing = 10;
+        };
+        "hyprland/workspaces" = {
+          format = "<sub>{icon}</sub>";
+          format-window-separator = " ";
         };
       };
     };
