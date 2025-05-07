341d524066180ef8409a950c3ca6cf46cd8d1e7e
mattephi <contact@mattephi.com>
2025-05-07 17:53:28 +0900

    fix: update ports and patch pools

--- hosts/ns-alpha/nixos/modules/pkgs.nix
+++ hosts/ns-alpha/nixos/modules/pkgs.nix
@@ -19,7 +19,10 @@
   networking.networkmanager.enable = true;
   networking.firewall = {
     enable = true;
-    allowedTCPPorts = [ 3366 ];
+    allowedTCPPorts = [
+      3366
+      8443
+    ];
   };
 
   environment.systemPackages = with pkgs; [
@@ -43,7 +46,20 @@
   systemd.services.mtprotoproxy.serviceConfig = {
     LoadCredential = [ "config:${config.sops.templates."config.py".path}" ];
     ExecStart = lib.mkForce ''
-      ${pkgs.mtprotoproxy}/bin/mtprotoproxy ''${CREDENTIALS_DIRECTORY}/config
+      ${
+        pkgs.mtprotoproxy.overrideAttrs (old: {
+          version = "latest";
+          patches = old.patches or [ ] ++ [
+            ../../../../overlays/modules/mtprotoproxy/0-disable-pool.patch
+          ];
+          src = pkgs.fetchFromGitHub {
+            owner = "alexbers";
+            repo = "mtprotoproxy";
+            rev = "bc841cff482a72472f15a36f86ca01aa11cac58b";
+            sha256 = "4wBHjtI0UYzmzLi8cESt4pjN1SgI5fwke8iwuaIVEFk=";
+          };
+        })
+      }/bin/mtprotoproxy ''${CREDENTIALS_DIRECTORY}/config
     '';
   };
 }
--- secrets/config.py
+++ secrets/config.py
@@ -1,5 +1,5 @@
 {
-	"data": "ENC[AES256_GCM,data:yfzxlJb+tmX2Ahsg4RqAnIK00ijUAjSTyYIwB+yPHbw/+Wfz9p3q/oWsGzisz1Q7CtpngfWlQm8U/eBz0ShQwVoipuVJDlVYg9RNvBz34Zhf2mPORa+QBfIirWJGKcUKNbHvjJrGEbbj3QJIa09VqKEy9oXsiCkpG6owiUnVLx6fdTE4PfgiQ53hsUivXR7gEkzk4CfITqOJvQbY4prC,iv:XDcAQu2TdIeDDu00LupenGMSWNFWB5vV9CD+3+WHO+Y=,tag:DEWACZ9TYCJF0UV7wJ+iaA==,type:str]",
+	"data": "ENC[AES256_GCM,data:4q3nP2oIG9+D1jNPqLK8YaxDh2t/8snyMWJTi8oeCPaJSGKmJL/zlXIqrHi9TSV3E9IkGHd4aRl0HPAaYdfRMhpuKeGwEQ8fHCGK6SKfrdLjyawwKzrFA9kZOx7ZmB+EQ3nkzG2xNYOVtWdfJAt6aZP+jIgKOxlf0BEEYZBeAe5R47pAJswLg0pXhWnEbR/+v+U6NW3goExc4IC1PfVx,iv:lZMHf8kMoucxZq7mrt3x56+dBNMFvjZYeQeBe27wXKk=,tag:JDEWZah+YsgeDNEupE3Pog==,type:str]",
 	"sops": {
 		"age": [
 			{
@@ -7,8 +7,8 @@
 				"enc": "-----BEGIN AGE ENCRYPTED FILE-----\nYWdlLWVuY3J5cHRpb24ub3JnL3YxCi0+IFgyNTUxOSBwSVdpWWNiN0JUTVB2dGFl\nVkxEdHdoSDAvRXB4MWZZamhBVVJOaVRGQXpNCkc0Mzd5S1lvakMzZld5K2I5bkZU\nWjdlMTI1NlF0aW9lWk1OY09wSmE0NTQKLS0tIDd1bk9lL21TNXVzelJtWEcxV0JP\nR2paYTNpYWFMeDBia3lpNm9BQjRzL3MKXidqxeQWBWNNtGiq4NClOY0gJrTahMo1\nFuCkiv94wE/MfMOM5WTo/CGXzW1ur1Xh8qqSMcy6iaWFD1szPxFc8g==\n-----END AGE ENCRYPTED FILE-----\n"
 			}
 		],
-		"lastmodified": "2025-05-04T19:05:31Z",
-		"mac": "ENC[AES256_GCM,data:TJFU8UuLHSDo4DWTPhBntLFZNdaaHjLZhF+Gt94xm1l/GLDfO4cH4AscuF9/vHiIMEFKtOdoN187DfYh3VWLXto4G8C8jvFrtanO5RsJkc3vhnjF+TsbnSL0EmIEZDjAMFywLQT5hi8K/qqhLAbNge/kxvBzhMFdfoXh3Y7ADVU=,iv:Ua5n7eYG09yzh6zh8beB0i7pVLgTJ8BbmpW5MAlID1o=,tag:oLDJXMRwaqh0Tof9zKMnGQ==,type:str]",
+		"lastmodified": "2025-05-05T07:15:50Z",
+		"mac": "ENC[AES256_GCM,data:6gZ6WvzHV5nuwm1mL/BYfINAucwV+y9gQxgDzh6GiLkPdZzV5CeuhyW0puki0BYWKPLrLfavmEq4PO+ineVonWvJwPr2F7qKlduEvBH5DkQZRdRxJGk5f11AMeQCot+Y1/gHxWJyBKRls3eMoM5LO1z6bFpp15j+T7BjlwnXPQo=,iv:E+ozqHHtIkEHryw/r2BfQ+TAKtYBgcoxUOzFos9Cozo=,tag:tkvXJJ7o2MZn+ItGhN3giA==,type:str]",
 		"unencrypted_suffix": "_unencrypted",
 		"version": "3.10.2"
 	}
