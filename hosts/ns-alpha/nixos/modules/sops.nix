cd5c874203979050be358d7f5406bbd12b5044b4
mattephi <contact@mattephi.com>
2025-06-01 21:51:08 +0900

    wip: alpha ai works in some sense

--- hosts/ns-alpha/nixos/modules/sops.nix
+++ hosts/ns-alpha/nixos/modules/sops.nix
@@ -12,6 +12,32 @@
         format = "binary";
         sopsFile = ../secrets/Caddyfile;
       };
+      "open-webui.env" = {
+        format = "dotenv";
+        sopsFile = ../secrets/open-webui.env;
+      };
+      searx = {
+        format = "binary";
+        sopsFile = ../secrets/searx;
+      };
+      "litellm" = {
+        format = "yaml";
+        sopsFile = ../secrets/litellm.yaml;
+        key = "";
+      };
+      "litellm.env" = {
+        format = "dotenv";
+        sopsFile = ../secrets/litellm.env;
+      };
+      "litellm.postgres.env" = {
+        format = "dotenv";
+        sopsFile = ../secrets/litellm.postgres.env;
+      };
+      "litellm.prometheus.yaml" = {
+        format = "yaml";
+        sopsFile = ../secrets/litellm.prometheus.yaml;
+        key = "";
+      };
     };
 
     templates = {
@@ -20,6 +46,12 @@
         owner = "caddy";
         content = ''${config.sops.placeholder."Caddyfile"}'';
       };
+      "open-webui.env".content = ''${config.sops.placeholder."open-webui.env"}'';
+      searx.content = ''${config.sops.placeholder.searx}'';
+      "litellm".content = ''${config.sops.placeholder."litellm"}'';
+      "litellm.env".content = ''${config.sops.placeholder."litellm.env"}'';
+      "litellm.postgres.env".content = ''${config.sops.placeholder."litellm.postgres.env"}'';
+      "litellm.prometheus.yaml".content = ''${config.sops.placeholder."litellm.prometheus.yaml"}'';
     };
   };
 }
